e02e701b23673d4e01fd9a91ddc5e181a16b932e
Lino2007 <44145624+Lino2007@users.noreply.github.com>
2019-11-12 23:36:24 +0100

    Finalizirani test fajlovi za rezervacije

--- tests/batch_scripts/runCheckRezervacijaGSPEC.bat
+++ tests/batch_scripts/runCheckRezervacijaGSPEC.bat
@@ -1,4 +1,4 @@
 cd ..
-call galen check mobileRezervacija.gspec --url http://localhost/wt1918048/html/mobileRezervacija.html --size 1263x975 --htmlreport htmlreports/mobileRezervacija-report
+call galen check mobileRezervacija.gspec --url http://localhost/wt1918048/html/rezervacija.html --size 1263x975 --htmlreport htmlreports/mobileRezervacija-report
 cd htmlreports/mobileRezervacija-report
 start report.html
