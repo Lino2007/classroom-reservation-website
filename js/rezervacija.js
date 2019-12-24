7e9de70a161275b045568a031f05165a632c1490
Lino2007 <44145624+Lino2007@users.noreply.github.com>
2019-12-25 00:58:10 +0100

    Prvi dio implementacije zadatka 2

--- js/rezervacija.js
+++ js/rezervacija.js
@@ -1,11 +1,30 @@
+
+var pocetak, kraj,opcija, trenutniMjesec;
 window.onload = (event) => {
     var d = new Date();
     Kalendar.iscrtajKalendar(document.getElementById("datumi"), d.getMonth());
     let arr= Pozivi.ucitajPodatkeIzJSON();
-  Kalendar.ucitajPodatke(arr[0], arr[1]);
+    Kalendar.ucitajPodatke(arr[0], arr[1]);
     Kalendar.ucitajPodatkeIzForme ();
 };
 
+function validirajFormu () {
+  return pocetak!="" && kraj!="" && pocetak<=kraj;
+}
+
+function ucitajFormu () {
+   pocetak = document.getElementById("pocetak").value;
+   kraj = document.getElementById("kraj").value;
+   opcija = document.getElementById("saleSelect").value;
+   trenutniMjesec = document.getElementById("month").textContent;
+   return validirajFormu();
+}
+
 function rezervirajTermin (odabraniDan) {
-    alert("Kliknut je kalendar za datum: " + odabraniDan);
+    if (!ucitajFormu()) { 
+        alert("Greška: Nespravni podaci u formi.");
+        return ;
+    }
+    let potvrda = confirm("Da li želite rezervisati odabrani termin?");
+    alert (potvrda ? "Prihvatili ste rezervaciju!" : "Niste prihvatili rezervaciju!");
 }
